3250408cfd65708b1ae91b5c6d71a9c69db2945b
takehiro1111 <t.soccer.0916@gmail.com>
2025-03-28 14:21:06 +0900

    update:メッセージを辞書で変数化

--- lesson/task1-23/main.py
+++ lesson/task1-23/main.py
@@ -10,7 +10,26 @@
 
 from dataclasses import dataclass
 
-from setting import ERROR_MESSAGE, GUIDE_MENU_MSG, GUIDE_NUMBER
+from setting import ATM_ID_MSG, ATM_PIN_MSG, ERROR_MESSAGE, GUIDE_MENU_MSG, GUIDE_NUMBER
+
+
+class AccountManager:
+    """銀行口座の属性情報を管理
+    認証に必要なユーザーIDと暗証番号を保持。
+
+    """
+
+    def account_auth_info(self) -> list[dict]:
+        """ユーザーごとにIDと暗証番号のリストをデータとして保持。
+
+        Returns:
+            list[dict]: ATMの処理に必要な情報
+        """
+        users = [
+            {"id": "takehiro1111", "pin": 1234},
+            {"id": "Michael", "pin": 5678},
+        ]
+        return users
 
 
 @dataclass
@@ -43,21 +62,76 @@ class BankAccount:
         self._balance = result_after_atm
 
 
-class Auth:
-    """ユーザーIDと暗証番号による認証機能"""
+class ATM:
+    """ATM
+    預金、引き出しの機能を持つ。
+    """
 
-    def __init__(self):
+    def __init__(self, bank_account: BankAccount, auth_manager=AccountManager) -> None:
+        self.bank_account = bank_account
         self.authenticated_user_id = None
+        self.auth_manager = auth_manager
 
-    @property
-    def user_list(self) -> list[dict]:
-        """ユーザーごとにIDと暗証番号のリストをデータとして保持。
+    def guide_menu(self, menu_num: int) -> int | list[str] | None:
+        """_summary_
+
+        Args:
+            menu_num (int): ATMの処理を選択する番号
 
         Returns:
-            list[dict]: ATMの処理に必要な情報
+            int | list[str] | None: メニュー選択後に入金または出勤処理の関数を返す。
         """
-        users = [{"id": "takehiro1111", "pin": 1234}, {"id": "test-user", "pin": 5678}]
-        return users
+        if menu_num == GUIDE_NUMBER["deposit"]:
+            return self.deposit(input(GUIDE_MENU_MSG["deposit"]))
+        elif menu_num == GUIDE_NUMBER["withdraw"]:
+            return self.withdrawal(input(GUIDE_MENU_MSG["withdraw"]))
+
+    def deposit(self, deposit_amount: str) -> int | bool:
+        """入金
+        Args:
+            deposit_amount (int): 入金額
+        Returns:
+            int: 残高
+        """
+        to_int_deposit_amount = int(deposit_amount)
+        deposit_validation = DepositValidation(to_int_deposit_amount)
+
+        if deposit_validation.validate() and self.auth_user_pin(
+            int(input(ATM_PIN_MSG["input_pin"]))
+        ):
+            self.bank_account.my_account_balance += to_int_deposit_amount
+            print(f"{to_int_deposit_amount}円を入金しました。")
+            return self.bank_account.my_account_balance
+
+        return self.show_error_msg(deposit_validation.errors)
+
+    def withdrawal(self, withdrawal_amount: str) -> int | None:
+        """出金
+        Args:
+            withdrawal_amount (int): 出金額
+        Returns:
+            int: 残高
+            str: 残高不足のメッセージ
+        """
+        to_int_withdraw_amount = int(withdrawal_amount)
+        withdraw_validation = WithdrawalValidation(
+            self.bank_account.my_account_balance, to_int_withdraw_amount
+        )
+
+        if withdraw_validation.validate() and self.auth_user_pin(
+            input(ATM_PIN_MSG["input_pin"])
+        ):
+            self.bank_account.my_account_balance -= to_int_withdraw_amount
+            print(f"{to_int_withdraw_amount}円を引き出しました。")
+            return self.bank_account.my_account_balance
+
+        return self.show_error_msg(withdraw_validation.errors)
+
+    @staticmethod
+    def show_error_msg(error_msg: list[str]) -> None:
+        for msg in error_msg:
+            print(msg)
+        return None
 
     @property
     def return_user_ids(self) -> list[str]:
@@ -66,7 +140,7 @@ class Auth:
         Returns:
             list[string]: 全ユーザーのID
         """
-        return [user["id"] for user in self.user_list]
+        return [user["id"] for user in self.auth_manager.account_auth_info(self)]
 
     def get_user_pin(self, user_id: str) -> int | None:
         """入力された暗証番号と比較するために保存されているそのユーザーの暗証番号を取得
@@ -77,7 +151,7 @@ class Auth:
         Returns:
             str | None: 登録済みのユーザーの場合に暗証番号を返す。
         """
-        for user in self.user_list:
+        for user in self.auth_manager.account_auth_info(self):
             if user_id == user["id"]:
                 return user["pin"]
 
@@ -94,7 +168,7 @@ class Auth:
             bool: ユーザーIDによる認証の成否
         """
         if attempt_check == 0:
-            print("ユーザーIDの入力回数を超過しました。最初からやり直してください。")
+            print(ATM_ID_MSG["exceed_limit_input_id"])
             return False
 
         if input_user_id in self.return_user_ids:
@@ -102,11 +176,9 @@ class Auth:
             return True
         elif input_user_id not in self.return_user_ids:
             attempt_check -= 1
-            return self.auth_user_id(
-                input("ユーザーiDを入力してください。(英文字 + 数字)"), attempt_check
-            )
+            return self.auth_user_id(input(ATM_ID_MSG["input_user_id"]), attempt_check)
 
-    def auth_user_pin(self, input_user_pin: int, attempt_check=3) -> bool:
+    def auth_user_pin(self, input_user_pin: str, attempt_check=3) -> bool:
         """暗証番号による認証
 
         Args:
@@ -117,100 +189,28 @@ class Auth:
             bool: 暗証番号による認証の成否
         """
         if attempt_check == 0:
-            print("暗証番号の入力回数を超過しました。最初からやり直してください。")
+            print(ATM_PIN_MSG["exceed_limit_input_pin"])
             return False
 
-        # 入力時に比較対象になる暗証番号の取得
-        correct_pin = self.get_user_pin(self.authenticated_user_id)
+        try:
+            to_int_input_user_pin = int(input_user_pin)
+            # 入力時に比較対象になる暗証番号の取得
+            correct_pin = self.get_user_pin(self.authenticated_user_id)
 
-        if input_user_pin == correct_pin:
-            print("正しい暗証番号です。")
-            return True
-        elif input_user_pin != correct_pin:
-            print("暗証番号が間違っています。再入力してください。")
+            if to_int_input_user_pin == correct_pin:
+                print(ATM_PIN_MSG["correct_input_pin"])
+                return True
+            elif to_int_input_user_pin != correct_pin:
+                print(ATM_PIN_MSG["mistake_input_pin"])
+                attempt_check -= 1
+                return self.auth_user_pin(
+                    input(ATM_PIN_MSG["input_pin"]), attempt_check
+                )
+
+        except ValueError:
+            print(ATM_PIN_MSG["type_err_input_pin"])
             attempt_check -= 1
-            return self.auth_user_pin(
-                int(input("暗証番号を入力してください。")), attempt_check
-            )
-
-
-class ATM:
-    """ATM
-    預金、引き出しの機能を持つ。
-    """
-
-    def __init__(self, bank_account: BankAccount, auth: Auth) -> None:
-        self.bank_account = bank_account
-        self.auth = auth
-
-    def guide_menu(self, menu_num: int) -> int | list[str] | None:
-        """_summary_
-
-        Args:
-            menu_num (int): ATMの処理を選択する番号
-
-        Returns:
-            int | list[str] | None: メニュー選択後に入金または出勤処理の関数を返す。
-        """
-        if menu_num == GUIDE_NUMBER["deposit"]:
-            return self.deposit(input(GUIDE_MENU_MSG["deposit"]))
-        elif menu_num == GUIDE_NUMBER["withdraw"]:
-            return self.withdrawal(input(GUIDE_MENU_MSG["withdraw"]))
-
-    def deposit(self, deposit_amount: str) -> int | None:
-        """入金
-        Args:
-            deposit_amount (int): 入金額
-        Returns:
-            int: 残高
-        """
-        to_int_deposit_amount = int(deposit_amount)
-        deposit_validation = DepositValidation(to_int_deposit_amount)
-        try:
-            if deposit_validation.validate() and self.auth.auth_user_pin(
-                int(input("暗証番号を入力してください。"))
-            ):
-                self.bank_account.my_account_balance += to_int_deposit_amount
-                print(f"{to_int_deposit_amount}円を入金しました。")
-                return self.bank_account.my_account_balance
-
-            return self.show_error_msg(deposit_validation.errors)
-
-        except ValueError:
-            print("暗証番号は数字4桁で入力してください。")
-            return None
-
-    def withdrawal(self, withdrawal_amount: str) -> int | None:
-        """出金
-        Args:
-            withdrawal_amount (int): 出金額
-        Returns:
-            int: 残高
-            str: 残高不足のメッセージ
-        """
-        to_int_withdraw_amount = int(withdrawal_amount)
-        withdraw_validation = WithdrawalValidation(
-            self.bank_account.my_account_balance, to_int_withdraw_amount
-        )
-        try:
-            if withdraw_validation.validate() and self.auth.auth_user_pin(
-                int(input("暗証番号を入力してください。"))
-            ):
-                self.bank_account.my_account_balance -= to_int_withdraw_amount
-                print(f"{to_int_withdraw_amount}円を引き出しました。")
-                return self.bank_account.my_account_balance
-
-            return self.show_error_msg(withdraw_validation.errors)
-
-        except ValueError:
-            print("暗証番号は数字4桁で入力してください。")
-            return None
-
-    @staticmethod
-    def show_error_msg(error_msg: list[str]) -> None:
-        for msg in error_msg:
-            print(msg)
-        return None
+            return self.auth_user_pin(input(ATM_PIN_MSG["input_pin"]), attempt_check)
 
 
 class BaseValidation:
@@ -298,17 +298,14 @@ def main() -> None:
     # 口座のインスタンス化
     bank_account = BankAccount(100)
 
-    # 認証機能のインスタンス化
-    auth = Auth()
-
     # ATM機能のインスタンス化
-    atm = ATM(bank_account, auth)
+    atm = ATM(bank_account)
 
     # 残金の確認
     print(f"残金:{bank_account.my_account_balance}")
 
     # ユーザーIDの認証
-    if auth.auth_user_id(input("ユーザーIDを入力してください。(英文字 + 数字)")):
+    if atm.auth_user_id(input(ATM_ID_MSG["input_user_id"])):
         # ATMの操作案内
         atm.guide_menu(int(input(GUIDE_MENU_MSG["front"])))
 
--- lesson/task1-23/setting.py
+++ lesson/task1-23/setting.py
@@ -15,3 +15,16 @@ GUIDE_MENU_MSG = {
     "deposit": "入金額を入力してください。",
     "withdraw": "出金額を入力してください。",
 }
+
+ATM_ID_MSG = {
+    "input_user_id": "ユーザーiDを入力してください。(英文字 + 数字)",
+    "exceed_limit_input_id": "ユーザーIDの入力回数を超過しました。最初からやり直してください。"
+}
+
+ATM_PIN_MSG = {
+    "input_pin": "暗証番号を入力してください。",
+    "type_err_input_pin": "暗証番号は数字4桁で入力してください。",
+    "mistake_input_pin": "暗証番号が間違っています。再入力してください。",
+    "correct_input_pin": "正しい暗証番号です。",
+    "exceed_limit_input_pin": "暗証番号の入力回数を超過しました。最初からやり直してください。",
+}
