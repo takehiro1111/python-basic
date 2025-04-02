1a715f72ac6413acc8b56847abf2d3cae86db9fd
takehiro1111 <t.soccer.0916@gmail.com>
2025-04-02 22:51:46 +0900

    update:ディレクトリの作成

--- lesson/task1-24/main.py
+++ lesson/task1-24/main.py
@@ -9,15 +9,21 @@ print("\n82. ファイルの作成")
 import os
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
-print(file_dir)
+mkdir = "test"
+test_dir = os.path.join(file_dir, mkdir)
 
-with open(f"{file_dir}/test.txt", mode="w") as f:
+# ディレクトリの作成
+os.makedirs(test_dir, exist_ok=True)
+
+absolute_path = os.path.join(test_dir, "test.txt")
+
+with open(absolute_path, mode="w") as f:
     f.write("Hello, Python!")
 
 # ### **83. ファイルの読み込み**
 print("\n83. ファイルの読み込み")
 # 問題 82 で作成した `test.txt` を開き、内容を読み取って出力するプログラムを書いてください。
-with open(f"{file_dir}/test.txt") as f:
+with open(absolute_path) as f:
     read = f.read()
 
 print(read)
@@ -26,10 +32,10 @@ print(read)
 print("\n84. ファイルの追記")
 # `test.txt` に `"Welcome to file handling in Python."` という一文を追加で書き込むプログラムを書いてください。
 # その後、ファイルの内容をすべて出力してください。
-with open(f"{file_dir}/test.txt", mode="a") as f:
+with open(absolute_path, mode="a") as f:
     f.write("\nWelcome to file handling in Python.")
 
-with open(f"{file_dir}/test.txt") as f:
+with open(absolute_path) as f:
     read = f.read()
 
 print(read)
@@ -37,6 +43,6 @@ print(read)
 # ### **85. ファイルの行ごとの読み込み**
 print("\n85. ファイルの行ごとの読み込み")
 # `test.txt` の内容を1行ずつ読み取り、各行をリストに格納し、そのリストを出力するプログラムを書いてください。
-with open(f"{file_dir}/test.txt") as f:
+with open(absolute_path) as f:
     lines_rstrip = [line.rstrip("\n") for line in f]
     print(lines_rstrip)
